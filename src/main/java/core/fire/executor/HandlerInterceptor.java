d91a0555bf536c2b5a260dad8031ffce255fe91b
lihuoliang <454654590@qq.com>
2016-06-15 09:30:26 +0800

    * 完善拦截器注释

--- src/main/java/core/fire/executor/HandlerInterceptor.java
+++ src/main/java/core/fire/executor/HandlerInterceptor.java
@@ -2,6 +2,13 @@ package core.fire.executor;
 
 import org.springframework.core.Ordered;
 
+/**
+ * TCP协议拦截器接口，实现此接口的拦截器将自动被{@linkplain DispatcherHandler}扫描并注册
+ * 
+ * @author lhl
+ *
+ *         2016年6月15日 上午9:28:46
+ */
 public interface HandlerInterceptor extends Ordered
 {
     /**
