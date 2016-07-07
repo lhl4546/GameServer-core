a7df3145ff5804ab3b624bfd7e530d73eacecfeb
lihuoliang <454654590@qq.com>
2016-07-07 11:59:19 +0800

    * 更新数值模版实现

--- src/main/java/core/fire/TemplateLoader.java
+++ src/main/java/core/fire/TemplateLoader.java
@@ -12,7 +12,7 @@ import core.fire.util.ClassUtil;
 import core.fire.util.Util;
 
 /**
- * 数值加载器
+ * 数值加载器。该加载器会从指定包下面加载所有符合条件的{@link Template}子类并调用这个类的{@code load}方法。
  * 
  * @author lihuoliang
  *
@@ -35,6 +35,21 @@ public enum TemplateLoader implements Template, Component {
 
     @Override
     public void load() {
+        loadTemplateClass();
+        invokeLoadMethod();
+    }
+
+    /**
+     * 调用Template的load方法
+     */
+    private void invokeLoadMethod() {
+        templates.values().forEach(template -> template.load());
+    }
+
+    /**
+     * 加载所有符合条件的Template子类
+     */
+    private void loadTemplateClass() {
         if (Util.isNullOrEmpty(scanPackages)) {
             throw new IllegalStateException("ScanPackages must be specified before load templates");
         }
@@ -44,7 +59,7 @@ public enum TemplateLoader implements Template, Component {
             try {
                 List<Class<?>> t = ClassUtil.getClasses(pack);
                 Map<Class<? extends Template>, Template> tmp = new HashMap<>();
-                t.stream().filter(cls -> isValidTemplate(cls)).map(prototype -> newInstance(prototype)).peek(instance -> tmp.put(instance.getClass(), instance)).forEach(template -> template.load());
+                t.stream().filter(cls -> isValidTemplate(cls)).map(prototype -> newInstance(prototype)).forEach(instance -> tmp.put(instance.getClass(), instance));
                 this.templates = tmp;
             } catch (Exception e) {
                 throw new RuntimeException(e);
@@ -52,8 +67,20 @@ public enum TemplateLoader implements Template, Component {
         }
     }
 
+    /**
+     * <ol>
+     * <li>是Template子类</li>
+     * <li>不是TemplateLoader类</li>
+     * <li>不是抽象类</li>
+     * <li>不是Template接口本身</li>
+     * </ol>
+     * 符合上述条件的类即是有效的数值模版类
+     * 
+     * @param prototype
+     * @return
+     */
     private boolean isValidTemplate(Class<?> prototype) {
-        return prototype != this.getClass() && !Modifier.isAbstract(prototype.getModifiers()) && !prototype.isInterface();
+        return Template.class.isAssignableFrom(prototype) && prototype != this.getClass() && !Modifier.isAbstract(prototype.getModifiers()) && !prototype.isInterface();
     }
 
     private Template newInstance(Class<?> prototype) {
@@ -67,6 +94,13 @@ public enum TemplateLoader implements Template, Component {
     @SuppressWarnings("unchecked")
     public <T extends Template> T get(Class<T> key) {
         return (T) templates.get(key);
+    }
+
+    /**
+     * 重新加载，运行期间调用该方法只能对已扫描的类执行load调用
+     */
+    public void reload() {
+        invokeLoadMethod();
     }
 
     @Override
