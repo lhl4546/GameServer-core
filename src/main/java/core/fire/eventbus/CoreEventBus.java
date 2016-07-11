056b87a3ef84c9395e8eed6b8c8a462d3a9e4245
lihuoliang <454654590@qq.com>
2016-07-11 22:49:49 +0800

    * 增加基于Guava的EventBus

---
+++ src/main/java/core/fire/eventbus/CoreEventBus.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package core.fire.eventbus;
+
+import com.google.common.eventbus.EventBus;
+
+/**
+ * 基于Guava的EventBus
+ * 
+ * @author lihuoliang
+ *
+ */
+public class CoreEventBus
+{
+    private static EventBus eventbus = new EventBus();
+
+    /**
+     * 抛出事件
+     * 
+     * @param event
+     */
+    public static void post(Object event) {
+        eventbus.post(event);
+    }
+
+    /**
+     * 注册时间处理器
+     * 
+     * @param object
+     */
+    public static void register(Object object) {
+        eventbus.register(object);
+    }
+}
