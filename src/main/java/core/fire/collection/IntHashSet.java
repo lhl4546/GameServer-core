6d60c41e244f686e3788bac20e303a9cf241b2f5
lihuoliang <454654590@qq.com>
2016-08-04 16:46:35 +0800

    * 更新inthashset实现

--- src/main/java/core/fire/collection/IntHashSet.java
+++ src/main/java/core/fire/collection/IntHashSet.java
@@ -3,19 +3,16 @@
  */
 package core.fire.collection;
 
-import java.util.AbstractCollection;
 import java.util.Iterator;
 
 /**
- * 基于IntHashMap实现的IntHashSet
- * <p>
- * 继承{@linkplain AbstractCollection}是为了支持序列化与反序列化，顺便也利用了它的{@code toString}功能
+ * IntHashSet which elements are primitive int
  * 
  * @author lhl
  *
  *         2016年5月24日 下午3:47:48
  */
-public class IntHashSet extends AbstractCollection<Integer>
+public class IntHashSet implements Iterable<Integer>
 {
     private transient IntHashMap<Boolean> map;
     private static final Boolean PRESENT = Boolean.TRUE;
@@ -24,34 +21,57 @@ public class IntHashSet extends AbstractCollection<Integer>
         map = new IntHashMap<>();
     }
 
+    /**
+     * Returns the number of elements in this hashset.
+     * 
+     * @return
+     */
     public int size() {
         return map.size();
     }
 
+    /**
+     * Tests if this hashset is empty
+     * 
+     * @return
+     */
     public boolean isEmpty() {
         return map.isEmpty();
     }
 
+    /**
+     * Tests if an specified element is in this hashset
+     * 
+     * @param e
+     * @return
+     */
     public boolean contains(int e) {
         return map.containsKey(e);
     }
 
+    /**
+     * add an element to this hashset
+     * 
+     * @param e
+     * @return
+     */
     public boolean add(int e) {
         return map.put(e, PRESENT) == null;
     }
 
     /**
-     * 为支持反序列化必须重写{@linkplain AbstractCollection#add(Object)}
-     * 方法，使用者请勿直接调用该方法，请使用重载方法{@linkplain #add(int)}添加元素
+     * remove an element from this hashset
+     * 
+     * @param e
+     * @return
      */
-    public boolean add(Integer e) {
-        return map.put(e, PRESENT) == null;
-    }
-
     public boolean remove(int e) {
         return map.remove(e) == PRESENT;
     }
 
+    /**
+     * remove all elements of this hashset
+     */
     public void clear() {
         map.clear();
     }
